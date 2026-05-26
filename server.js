5d32688c4bf320904524a220dcaca0a8d2846efa
VisaAndVoyage <tech.visaandvoyage@gmail.com>
2026-05-26 16:39:18 +0530

    feat: add root server.js entry point and update package.json for Hostinger deployment

---
+++ server.js
@@ -0,0 +1,3 @@
+// This file acts as the entry point for hosting providers like Hostinger or cPanel Passenger
+// which expect the main startup file to reside in the root directory.
+require('./server/server.js');
